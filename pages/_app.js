925800f6f2dc463884ba03574ab55eba07da606f
shujaakbar2020 <shujaakbar2020@gmail.com>
2022-12-20 19:14:14 +0500

    Revert "Added google analytics"
    This reverts commit 82ea95979dde12d66d347e3b1d21403ea1df1aee.

--- pages/_app.js
+++ pages/_app.js
@@ -3,25 +3,9 @@ import "../styles/argon-design-system-react.css";
 import "../styles/styles.css";
 import "../styles/vendor/font-awesome/css/font-awesome.min.css";
 import "../styles/vendor/nucleo/css/nucleo.css";
-import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
-	return (
-		<>
-			<Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=UA-252525820-1" />
-			{/* <!-- Google tag (gtag.js) --> */}
-			<script async src="https://www.googletagmanager.com/gtag/js?id=UA-252525820-1"></script>
-			<script>
-				window.dataLayer = window.dataLayer || [];
-				function gtag(){dataLayer.push(arguments)}
-				gtag('js', new Date());
-
-				gtag('config', 'UA-252525820-1');
-			</script>
-
-			<Component {...pageProps} />
-		</>
-	);
+	return <Component {...pageProps} />;
 }
 
 export default MyApp;
